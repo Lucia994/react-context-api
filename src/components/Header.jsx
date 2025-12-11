2f874735ae22d6d7eea2f169dbdca5390642d7c8
Lucia994 <l.summa94@gmail.com>
2025-12-11 16:18:35 +0100

    Create NavBar component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,9 @@
-import { NavLink } from "react-router-dom";
-const navLinks = [
-  {
-    title: "HomePage",
-    path: "/",
-  },
-  {
-    title: "About us",
-    path: "/about-us",
-  },
-  {
-    title: "Products",
-    path: "/products",
-  },
-];
+import NavBar from "./NavBar";
 
 export default function Header() {
   return (
     <header className="d-flex justify-content-end">
-      <ul className="d-flex list-unstyled align-items-center px-4 py-3">
-        {navLinks.map((link, index) => (
-          <li className="me-3" key={index}>
-            <NavLink to={link.path}>{link.title}</NavLink>
-          </li>
-        ))}
-      </ul>
+      <NavBar />
     </header>
   );
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,28 @@
+import { NavLink } from "react-router-dom";
+const navLinks = [
+  {
+    title: "HomePage",
+    path: "/",
+  },
+  {
+    title: "About us",
+    path: "/about-us",
+  },
+  {
+    title: "Products",
+    path: "/products",
+  },
+];
+export default function NavBar() {
+  return (
+    <>
+      <ul className="d-flex list-unstyled align-items-center px-4 py-3">
+        {navLinks.map((link, index) => (
+          <li className="me-3" key={index}>
+            <NavLink to={link.path}>{link.title}</NavLink>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
